f1742031efbc1e18b50ed0e2f677a601960c02ad
Matthew Magin <matthew.d.magin@gmail.com>
2019-05-15 16:31:02 +1000

    Implemented fill function and close polygon right click

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -45,6 +46,9 @@ public class Controller implements Initializable{
     @FXML private AnchorPane canvasPane;
     @FXML private AnchorPane canvasAnchorPane;
 
+    Color lineColor;
+    Color fillColor;
+
     boolean toolSelected = false;
     String selectedTool;
 
@@ -66,26 +70,12 @@ public class Controller implements Initializable{
     Canvas tempDrawingLayer;
     GraphicsContext tempDrawingLayerGC;
 
-
-    @FXML
-    private void onMousePressedListener(MouseEvent e){
-        System.out.println("mousepress");
-
-
-        //brush.setLineWidth(5);
-        //brush.setStroke(colourSelector.getValue());
-        //brush.clearRect(0, 0, canvasGo.getWidth() , canvasGo.getHeight());
-        //brush.strokeLine(10, 10, 100, 100);
-    }
-
     @FXML
     private void penToolClick(){
 
         changeActiveButton("pen");
         this.selectedTool = "plot";
         handleMouseEvent();
-
-
     }
 
 
@@ -95,7 +85,6 @@ public class Controller implements Initializable{
         changeActiveButton("line");
         this.selectedTool = "line";
         handleMouseEvent();
-
     }
 
     @FXML
@@ -122,7 +111,14 @@ public class Controller implements Initializable{
     @FXML
     private void closePolygon(){
         canvasAnchorPane.getChildren().remove(tempDrawingLayer);
+
+        brush.setStroke(lineColor);
+        brush.setFill(fillColor);
+
+        brush.setLineWidth(3);
+
         brush.strokePolygon(polygonPointsX, polygonPointsY, polygonPointsCount);
+        brush.fillPolygon(polygonPointsX, polygonPointsY, polygonPointsCount);
 
         polygonPointsX = new double[9999];
         polygonPointsY = new double[9999];
@@ -180,22 +176,36 @@ public class Controller implements Initializable{
         line.setStartY(y);
     }
 
+
     private void renderLinePreview(Double x, Double y){
         canvasAnchorPane.getChildren().remove(tempDrawingLayer);
         tempDrawingLayer = new Canvas(700, 500);
         canvasAnchorPane.getChildren().add(tempDrawingLayer);
         tempDrawingLayerGC = tempDrawingLayer.getGraphicsContext2D();
-        //tempDrawingLayerGC.setStroke(colourSelector.getValue());
 
         line.setEndX(x);
         line.setEndY(y);
+
+        tempDrawingLayerGC.setStroke(lineColor);
+        tempDrawingLayerGC.setLineWidth(1);
 
         tempDrawingLayerGC.setStroke(lineColorPicker.getValue());
         tempDrawingLayerGC.strokeLine(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
     }
 
+    private void plotPoint(Double x, Double y){
+        brush.setStroke(lineColor);
+        brush.setLineWidth(1);
+
+        brush.strokeLine(x, y, x, y);
+    }
+
     private void endLine(){
         canvasAnchorPane.getChildren().remove(tempDrawingLayer);canvasAnchorPane.getChildren().remove(tempDrawingLayer);
+
+        brush.setStroke(lineColor);
+        brush.setLineWidth(1);
+
         brush.strokeLine(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
         line = new Line();
     }
@@ -222,8 +232,12 @@ public class Controller implements Initializable{
         double[] xPoints = {startX, endX, endX, startX};
         double[] yPoints = {startY, startY, endY, endY};
 
-        tempDrawingLayerGC.setStroke(lineColorPicker.getValue());
+        tempDrawingLayerGC.setStroke(lineColor);
+        tempDrawingLayerGC.setFill(fillColor);
+        tempDrawingLayerGC.setLineWidth(3);
+
         tempDrawingLayerGC.strokePolygon(xPoints, yPoints, 4);
+        tempDrawingLayerGC.fillPolygon(xPoints, yPoints, 4);
 
         rectangle.setX(startX);
         rectangle.setY(startY);
@@ -232,20 +246,21 @@ public class Controller implements Initializable{
     private void endRectangle(Double x, Double y){
         canvasAnchorPane.getChildren().remove(tempDrawingLayer);
 
-        rectangle.setWidth(x - rectangle.getX());
-        rectangle.setHeight(y - rectangle.getY());
+        double startX = rectangle.getX();
+        double startY = rectangle.getY();
+        double endX = x;
+        double endY = y;
 
-        if(rectangle.getWidth() < 0){
-            rectangle.setWidth(rectangle.getWidth()*-1.0);
-            rectangle.setX(rectangle.getX() - rectangle.getWidth());
-        }
+        double[] xPoints = {startX, endX, endX, startX};
+        double[] yPoints = {startY, startY, endY, endY};
 
-        if(rectangle.getHeight() < 0){
-            rectangle.setHeight(rectangle.getHeight()*-1.0);
-            rectangle.setY(rectangle.getY() - rectangle.getHeight());
-        }
+        brush.setStroke(lineColor);
+        brush.setFill(fillColor);
+        brush.setLineWidth(3);
 
-        brush.strokeRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
+        brush.strokePolygon(xPoints, yPoints, 4);
+        brush.fillPolygon(xPoints, yPoints, 4);
+
         rectangle = new Rectangle();
     }
 
@@ -275,8 +290,12 @@ public class Controller implements Initializable{
         ellipseBounds.setWidth(Math.abs(x - ellipseBounds.getX()));
         ellipseBounds.setHeight(Math.abs(y - ellipseBounds.getY()));
 
-        tempDrawingLayerGC.setStroke(lineColorPicker.getValue());
+        tempDrawingLayerGC.setStroke(lineColor);
+        tempDrawingLayerGC.setFill(fillColor);
+        tempDrawingLayerGC.setLineWidth(3);
+
         tempDrawingLayerGC.strokeOval(Math.min(ellipseBounds.getX(), x), Math.min(ellipseBounds.getY(), y), ellipseBounds.getWidth(), ellipseBounds.getHeight());
+        tempDrawingLayerGC.fillOval(Math.min(ellipseBounds.getX(), x), Math.min(ellipseBounds.getY(), y), ellipseBounds.getWidth(), ellipseBounds.getHeight());
     }
 
     private void endEllipse(Double x, Double y){
@@ -285,13 +304,19 @@ public class Controller implements Initializable{
         ellipseBounds.setWidth(Math.abs(x - ellipseBounds.getX()));
         ellipseBounds.setHeight(Math.abs(y - ellipseBounds.getY()));
 
+        brush.setStroke(lineColor);
+        brush.setFill(fillColor);
+        brush.setLineWidth(3);
+
         brush.strokeOval(Math.min(ellipseBounds.getX(), x), Math.min(ellipseBounds.getY(), y), ellipseBounds.getWidth(), ellipseBounds.getHeight());
+        brush.fillOval(Math.min(ellipseBounds.getX(), x), Math.min(ellipseBounds.getY(), y), ellipseBounds.getWidth(), ellipseBounds.getHeight());
+
     }
 
     private void polygonClick(){
         canvasAnchorPane.setOnMouseClicked(event -> {
             if(selectedTool == "polygon") {
-                renderPolygonPreview(event.getX(), event.getY());
+                renderPolygonPreview(event.getX(), event.getY(), event.getButton());
             }
         });
     }
@@ -308,40 +333,46 @@ public class Controller implements Initializable{
         polygonClick();
     }
 
-    private void renderPolygonPreview(Double x, Double y){
+    private void renderPolygonPreview(Double x, Double y, MouseButton button){
+        if(button == MouseButton.SECONDARY){
+            closePolygon();
+            return;
+        }
+
         polygonPointsX[polygonPointsCount] = x;
         polygonPointsY[polygonPointsCount] = y;
+
+        tempDrawingLayerGC.setStroke(lineColor);
+        tempDrawingLayerGC.setLineWidth(1);
 
         tempDrawingLayerGC.setStroke(lineColorPicker.getValue());
         tempDrawingLayerGC.strokeLine(polygonPointsX[polygonPointsCount-1], polygonPointsY[polygonPointsCount-1], polygonPointsX[polygonPointsCount], polygonPointsY[polygonPointsCount]);
         polygonPointsCount++;
     }
 
-
-
-    @FXML private void endPolygon(){
-        canvasAnchorPane.getChildren().remove(tempDrawingLayer);
-
-        brush.strokePolygon(polygonPointsX, polygonPointsY, polygonPointsCount);
-        polygonPointsX = new double[9999];
-        polygonPointsY = new double[9999];
-        polygonPointsCount = 0;
+    private void refreshColors(){
+        lineColor = lineColorPicker.getValue();
+        fillColor = fillColorPicker.getValue();
     }
 
     private void handleMouseEvent(){
         canvas.setOnMouseClicked(event -> {
+            refreshColors();
+            if(selectedTool == "plot") {
+                plotPoint(event.getX(), event.getY());
+            }
+
             if(selectedTool == "polygon") {
                 if(polygonPointsCount == 0){
                     setupPolygon(event.getX(), event.getY());
                 } else {
-                    renderPolygonPreview(event.getX(), event.getY());
+                    renderPolygonPreview(event.getX(), event.getY(), event.getButton());
                 }
             }
         });
 
         canvas.setOnMousePressed(event -> {
-            brush.setStroke(lineColorPicker.getValue());
-            brush.setFill(Color.RED);
+            refreshColors();
 
             if(selectedTool == "line") {    setupLine(event.getX(), event.getY());  }
 
@@ -355,6 +386,8 @@ public class Controller implements Initializable{
         });
 
         canvas.setOnMouseDragged(event -> {
+            refreshColors();
+
             if(selectedTool == "line") {    renderLinePreview(event.getX(), event.getY());  }
 
             if(selectedTool == "rectangle"){
@@ -367,6 +400,8 @@ public class Controller implements Initializable{
         });
 
         canvas.setOnMouseReleased(event->{
+            refreshColors();
+
             if(selectedTool == "line") {    endLine();  }
 
             if(selectedTool == "rectangle"){
@@ -377,94 +412,6 @@ public class Controller implements Initializable{
                 endEllipse(event.getX(), event.getY());
             }
         });
-
-
-
-                /*canvas.setOnMouseClicked(event -> {
-                    brush.setStroke(lineColorPicker.getValue());
-                    if(selectedTool == "plot"){
-                        brush.beginPath();
-                        brush.lineTo(event.getX(), event.getY());
-                        brush.stroke();
-                    }
-
-
-                });*/
-                /*canvas.setOnMousePressed(event -> {
-                    brush.setStroke(lineColorPicker.getValue());
-
-                    if(selectedTool == "line") {
-                        brush.beginPath();
-                        line.setStartX(event.getX());
-                        line.setStartY(event.getY());
-                    }
-
-                    if(selectedTool == "rectangle"){
-                        rectangle.setX(event.getX());
-                        rectangle.setY(event.getY());
-                    }
-
-                    if(selectedTool == "ellipse"){
-                        ellipseBounds.setX(event.getX());
-                        ellipseBounds.setY(event.getY());
-                    }
-
-                    if(selectedTool == "polygon"){
-                        double[] xPoints = {300.0, 450.0, 300.0, 150.0};
-                        double[] yPoints = {50.0, 150.0, 250.0, 150.0};
-                        polygonXPoints[polygonPoints] = event.getX();
-                        polygonYPoints[polygonPoints] = event.getY();
-                        polygonPoints++;
-                    }
-
-                });*/
-
-                /*canvas.setOnMouseReleased(event->{
-                    if(selectedTool == "line") {
-                        line.setEndX(event.getX());
-                        line.setEndY(event.getY());
-                        brush.strokeLine(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
-                        brush.stroke();
-                    }
-
-                    if(selectedTool == "rectangle"){
-                        rectangle.setWidth(event.getX() - rectangle.getX());
-                        rectangle.setHeight(event.getY() - rectangle.getY());
-
-                        if(rectangle.getWidth() < 0){
-                            rectangle.setWidth(rectangle.getWidth()*-1.0);
-                            rectangle.setX(rectangle.getX() - rectangle.getWidth());
-                        }
-
-                        if(rectangle.getHeight() < 0){
-                            rectangle.setHeight(rectangle.getHeight()*-1.0);
-                            rectangle.setY(rectangle.getY() - rectangle.getHeight());
-                        }
-
-                        brush.strokeRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
-                    }
-
-                    if(selectedTool == "ellipse"){
-                        ellipseBounds.setWidth(event.getX() - ellipseBounds.getX());
-                        ellipseBounds.setHeight(event.getY() - ellipseBounds.getY());
-
-                        if(ellipseBounds.getWidth() < 0){
-                            ellipseBounds.setWidth(ellipseBounds.getWidth()*-1.0);
-                            ellipseBounds.setX(ellipseBounds.getX() - ellipseBounds.getWidth());
-                        }
-
-                        if(ellipseBounds.getHeight() < 0){
-                            ellipseBounds.setHeight(ellipseBounds.getHeight()*-1.0);
-                            ellipseBounds.setY(ellipseBounds.getY() - ellipseBounds.getHeight());
-                        }
-
-                        brush.strokeOval(ellipseBounds.getX(), ellipseBounds.getY(), ellipseBounds.getWidth(), ellipseBounds.getHeight());
-                    }
-                });*/
-
-
-
-
     }
 
 
@@ -476,46 +423,11 @@ public class Controller implements Initializable{
         brush = canvas.getGraphicsContext2D();
         brush.setLineWidth(1);
 
-
         if(selectedTool != null){
 
         } else {
             System.out.println("Please Select a Tool");
         }
-
-
-        /*brush = canvas.getGraphicsContext2D();
-        brush.setLineWidth(1);
-
-        canvas.setOnMouseClicked(event -> {
-            //double x = event.getX(), y = event.getY();
-            System.out.println("Canvas Clicked");
-            double coordX = event.getX();
-            double coordY = event.getY();
-
-            System.out.println(coordX);
-            System.out.println(coordY);
-
-            brush.beginPath();
-            brush.lineTo(coordX, coordY);
-            brush.stroke();
-
-        });*/
-        //canvas.setOnMouseDragged(e -> {
-//            double brushSize = Double.parseDouble(brushSizeInput.getText());
-//
-//            double mouseX = e.getX() - brushSize / 2;
-//            double mouseY = e.getY() - brushSize / 2;
-//
-//            if(toolSelected && !brushSizeInput.getText().isEmpty()){
-//                brush.setFill(colourSelector.getValue());
-//                brush.fillRoundRect(mouseX, mouseY, brushSize, brushSize, brushSize, brushSize);
-//            }
-        //});
-
-
-
-
     }
 
     @FXML
